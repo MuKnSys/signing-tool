b89448f8d1882405fbb6d65023cb97c7b3e00ef3
Vibhu Mohindra <vibs29@hotmail.com>
2022-11-23 05:27:57 +0100

    documentation

--- siggy/src/net/msg.go
+++ siggy/src/net/msg.go
@@ -2,7 +2,7 @@ package main
 
 type Message struct { /* value object */
   SenderIndex int
-  ProtocolCode protocol_code
+  ProtocolCode protocol_code //TODO design: type is defined in a higher layer
   RecipientIndex int
   RunId string
   Op int
